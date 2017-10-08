71c2035f9a345718529f250744f8c572b3e164c1
姜小萌 <1005638804@qq.com>
2017-10-08 15:01:24 +0800

    Create LogisticRegression.py

---
+++ CNN/LogisticRegression.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+
+__docformat__ = 'restructedtext en'
+
+import six.moves.cPickle as pickle
+import gzip
+import os
+import sys
+import timeit
+
+import numpy
+
+import theano
+import theano.tensor as T
+
+
+class LogisticRegression(object):
+
+    def __init__(self, input, n_in, n_out):
+        # start-snippet-1
+        # initialize with 0 the weights W as a matrix of shape (n_in, n_out)
+        self.W = theano.shared(
+            value=numpy.zeros(
+                (n_in, n_out),
+                dtype=theano.config.floatX
+            ),
+            name='W',
+            borrow=True
+        )
+        # initialize the biases b as a vector of n_out 0s
+        self.b = theano.shared(
+            value=numpy.zeros(
+                (n_out,),
+                dtype=theano.config.floatX
+            ),
+            name='b',
+            borrow=True
+        )
+
+       
+        self.p_y_given_x = T.nnet.softmax(T.dot(input, self.W) + self.b)
+
+        
+        self.y_pred = T.argmax(self.p_y_given_x, axis=1)
+       
+        
+        self.params = [self.W, self.b]
+
+        # keep track of model input
+        self.input = input
+
+    def negative_log_likelihood(self, y):
+
+    def errors(self, y):
+        # check if y has same dimension of y_pred
+        if y.ndim != self.y_pred.ndim:
+            raise TypeError(
+                'y should have the same shape as self.y_pred',
+                ('y', y.type, 'y_pred', self.y_pred.type)
+            )
+        # check if y is of the correct datatype
+        if y.dtype.startswith('int'):
+            # the T.neq operator returns a vector of 0s and 1s, where 1
+            # represents a mistake in prediction
+            return T.mean(T.neq(self.y_pred, y))
+        else:
+            raise NotImplementedError()
+
+
